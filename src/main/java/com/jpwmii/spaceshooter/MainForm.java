a7f021c20939a406d5cb26d6dafbadc6888726b9
ikucharski <igorkucharski42@gmail.com>
2023-11-23 21:08:47 +0100

    Changed the way background is animated, added background image scaling to fit the window

--- src/main/java/com/jpwmii/spaceshooter/MainForm.java
+++ src/main/java/com/jpwmii/spaceshooter/MainForm.java
@@ -2,41 +2,28 @@ package com.jpwmii.spaceshooter;
 
 import com.jpwmii.spaceshooter.components.BackgroundComponent;
 import javax.swing.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.*;
 
 public class MainForm extends JFrame {
-    private int width;
-    private int height;
     private JPanel mainPanel;
     private BackgroundComponent backgroundComponent;
 
     public MainForm(int width, int height) {
-        this.width = width;
-        this.height = height;
         this.setTitle("Space Shooter");
         this.setSize(width, height);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         createUIComponents();
-
         this.setVisible(true);
-
     }
 
     private void createUIComponents() {
         //Main panel
         this.mainPanel = new JPanel();
         this.add(mainPanel);
-        mainPanel.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                super.componentResized(e);
-            }
-        });
 
         //Background component
-        this.backgroundComponent = new BackgroundComponent(this.width, this.height);
+        backgroundComponent = new BackgroundComponent();
+        backgroundComponent.setPreferredSize(new Dimension(getWidth(), getHeight()));
         mainPanel.add(backgroundComponent);
     }
 }
--- src/main/java/com/jpwmii/spaceshooter/components/BackgroundComponent.java
+++ src/main/java/com/jpwmii/spaceshooter/components/BackgroundComponent.java
@@ -1,40 +1,65 @@
 package com.jpwmii.spaceshooter.components;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
 
-public class BackgroundComponent extends JComponent {
-    private int width;
-    private int height;
-    private ImageIcon backgroundImage;
+public class BackgroundComponent extends JComponent implements ActionListener {
+    private BufferedImage backgroundSpriteSheet;
+    private BufferedImage backgroundFrame;
+    private int frameIndex = 0;
+    private final int frameCount = 40;
+    private final int frameWidth = 560;
+    private final int frameHeight = 498;
+    private final int animationSpeed = 50;
 
-    public BackgroundComponent(int width, int height) {
-        this.width = width;
-        this.height = height;
-        loadBackground();
-        setPreferredSize(new Dimension(width, height));
+    public BackgroundComponent() {
+        loadBackgroundImage();
+        Timer timer = new Timer(animationSpeed, this);
+        timer.start();
     }
 
-    private void loadBackground() {
-        URL imageUrl = getClass().getResource("/background.gif");
+    private void loadBackgroundImage() {
+        URL imageUrl = getClass().getResource("/background-sprite-sheet.png");
 
         if (imageUrl != null) {
-            this.backgroundImage = new ImageIcon(imageUrl);
+            try {
+                this.backgroundSpriteSheet = ImageIO.read(imageUrl);
+            }
+            catch(IOException exception) {
+                System.err.println("Error while reading from background file:\n" + exception.getLocalizedMessage());
+            }
         } else {
             System.err.println("Failed to load the animated background image.");
         }
     }
 
-    //TODO: rescale image icon if bigger than window
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (backgroundFrame != null) {
+            int parentWidth = getParent().getWidth();
+            int parentHeight = getParent().getHeight();
+            setPreferredSize(new Dimension(parentWidth, parentHeight));
+            g.drawImage(backgroundFrame, 0, 0, parentWidth, parentHeight, this);
+        }
+    }
 
     @Override
-    protected void paintComponent(Graphics graphics) {
-        super.paintComponent(graphics);
+    public void actionPerformed(ActionEvent e) { // Executes every timer tick
+        if(frameIndex < frameCount - 1)
+            frameIndex++;
+        else
+            frameIndex = 0;
 
-        if (backgroundImage != null) {
-            //backgroundImage.paintIcon(this, graphics, 0, 0);
-            graphics.drawImage(backgroundImage.getImage(), 0, 0, this.width, this.height, this);
-        }
+        backgroundFrame = backgroundSpriteSheet.getSubimage(frameIndex * frameWidth, 0, frameWidth, frameHeight);
+
+        repaint();
     }
 }
